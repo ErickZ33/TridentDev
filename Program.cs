2f10d23f90bea79ff1bf63651e1e2ebdf4df8d5d
Erick Ruiz <erick_ruiz23@yahoo.com>
2018-04-29 13:04:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace TridentDev2
+{
+    public class Program
+    {
+public static void Main(string[] args) { BuildWebHost(args).Run(); } public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args) .UseStartup<Startup>() .Build(); }}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+// Write your Javascript code.
+$(document).ready(function () {
+
+    $('.parallax').parallax();
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-120px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+    
+});
